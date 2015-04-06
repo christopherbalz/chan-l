6188cdb7452b828edd06e39c145c17ca7ca85cb6
Chris Balz <cbalz@apple.com>
2015-04-05 23:18:58 -0700

    Added cursor protection.

--- src/ChanL.js
+++ src/ChanL.js
@@ -4,26 +4,26 @@ var Baobab = require('baobab'),
 
 module.exports = {   
 
-    
-    init: function(p_componentCursorMapping) {
-        componentCursorMapping = p_componentCursorMapping;
+  
+  init: function( p_componentCursorMapping ) {
+    componentCursorMapping = p_componentCursorMapping;
 
-        if( !componentCursorMapping ) {
+    if( !componentCursorMapping ) {
 
-            console.warn( 'No component cursor mapping defined.' );
-        }
-    },
-
-
-    getCursor: function(component, subpath) {
-        var root;
-        
-        if( !componentCursorMapping[ component.displayName ] ) {
-
-            throw new Error( 'No cursor access defined in `componentCursorMapping` for component with displayName ' + component.displayName );
-        }
-        root = componentCursorMapping[ component.displayName ];
-        
-        return appCursor.select( root + subpath );
+      console.warn( 'No component cursor mapping defined.' );
     }
+  },
+
+
+  getCursor: function( component ) { // No path input is provided here since we want to lock it to the original cursor-mapping.
+    var root;
+    
+    if( !componentCursorMapping[ component.displayName ] ) {
+
+      throw new Error( 'No cursor access defined in `componentCursorMapping` for component with displayName ' + component.displayName );
+    }
+    root = componentCursorMapping[ component.displayName ];
+    
+    return appCursor.select( root );
+  }
 };
